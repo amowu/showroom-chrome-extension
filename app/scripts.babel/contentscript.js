d696994fa5f98345c95aab93ef9c8822abb269c4
Amo Wu <amo260@gmail.com>
2016-11-20 14:01:40 +0800

    feat: new custom HTML5 video player
    Use HTML5 player and HLS streaming to replace Showroom default Flash player

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,50 @@
-'use strict';
+'use strict'
 
-console.log('\'Allo \'Allo! Content script');
+console.log('\'Allo \'Allo! Content script')
+
+const FLASH_ID = 'ShowRoomLive'
+const roomUrlKey = window.location.pathname.split('/')[1]
+const requestUrl = `https://www.showroom-live.com/api/room/status?room_url_key=${roomUrlKey}&=${Date.now()}`
+
+fetch(requestUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      const {
+        status,
+        statusText,
+        url
+      } = response
+
+      return Promise.reject(new Error(`${url} ${status} ${statusText}`))
+    }
+  })
+  .then(json => {
+    const {
+      streaming_url_hls: streamingUrlHls
+    } = json
+
+    const flash = document.getElementById(FLASH_ID)
+    const video = document.createElement('video')
+
+    video.setAttribute('width', '100%')
+    video.setAttribute('height', '100%')
+    video.setAttribute('controls', '')
+
+    flash.parentNode.replaceChild(video, flash)
+
+    const hls = new Hls();
+    
+    hls.loadSource(streamingUrlHls)
+    hls.attachMedia(video)
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      video.play()
+    })
+    hls.on(Hls.Events.ERROR, (event, data) => {
+      console.log(`${data.type} ${data.details}`)
+    })
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
